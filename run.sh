acd2b0645ae82361f558e06c21347a99d24a5650
Neil Seligmann <git@neilseligmann.com>
2025-04-18 15:30:59 -0300

    feat: will now use envs for the fifo

--- run.sh
+++ run.sh
@@ -15,8 +15,15 @@ echo 0 > /proc/sys/fs/protected_fifos
 sysctl fs.protected_fifos=0
 
 echo "Creating fifo"
-pactl load-module module-pipe-sink file=/tmp/audio/plexamp_fifo sink_name=Plexamp format=s16le rate=48000
-chmod 777 /tmp/audio/plexamp_fifo
+
+# Use environment variables with defaults
+PIPE_FILE=${PIPE_FILE:-/tmp/audio/plexamp_fifo}
+SINK_NAME=${SINK_NAME:-Plexamp}
+FORMAT=${FORMAT:-s16le}
+RATE=${RATE:-48000}
+
+pactl load-module module-pipe-sink file=${PIPE_FILE} sink_name=${SINK_NAME} format=${FORMAT} rate=${RATE}
+chmod 777 ${PIPE_FILE}
 
 echo "List sinks"
 pactl list short sinks
