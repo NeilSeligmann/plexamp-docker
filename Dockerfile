53f34392bab30c7e39aaf454fedc4cd9fcc73507
Neil Seligmann <git@neilseligmann.com>
2025-04-19 02:21:27 -0300

    feat: updated and fixed run.sh

--- Dockerfile
+++ Dockerfile
@@ -26,22 +26,26 @@ RUN apt update -y && apt install -y -q \
 ENV WORKDIR /plexamp
 
 RUN adduser root pulse-access
+# RUN adduser node pulse-access
 
 RUN mkdir -p $WORKDIR
-RUN chown -R $UNAME:$UNAME $WORKDIR
-USER $UNAME
+RUN chown -R root:root $WORKDIR
+USER root
 
 WORKDIR $WORKDIR
 RUN wget -q "$(curl -s "https://plexamp.plex.tv/headless/version$1.json" | jq -r '.updateUrl')" -O plexamp.tar.bz2
 RUN tar xfj plexamp.tar.bz2
 
-
 ENV WORKDIR $WORKDIR/plexamp
 WORKDIR $WORKDIR
 
-COPY ./run.sh $WORKDIR/run.sh
-RUN chmod +x $WORKDIR/run.sh
+COPY --chown=root:root --chmod=755 ./run.sh $WORKDIR/run.sh
+# RUN chmod +x $WORKDIR/run.sh
 
-# USER node
+# Disable protected fifos
+# RUN echo 0 > /proc/sys/fs/protected_fifos
+# RUN sysctl fs.protected_fifos=0
+
+USER root
 
 ENTRYPOINT sh -c $WORKDIR/run.sh
--- run.sh
+++ run.sh
@@ -1,8 +1,12 @@
+#!/bin/bash
+
+set -e
+
 echo "Running as user: $(whoami)"
 
 echo "Starting Pulseaudio"
 # pulseaudio -D --verbose --exit-idle-time=-1
-pulseaudio -D --verbose --exit-idle-time=-1 --system --disallow-exit
+pulseaudio -D -vvvv --exit-idle-time=-1 --system --disallow-exit
 # systemctl status --user pipewire-pulse.service
 
 # pacmd load-module module-virtual-sink sink_name=v1
@@ -10,20 +14,30 @@ pulseaudio -D --verbose --exit-idle-time=-1 --system --disallow-exit
 # pactl -help
 # pactl list short sinks
 
-echo "Disabling protected fifos"
-echo 0 > /proc/sys/fs/protected_fifos
-sysctl fs.protected_fifos=0
-
-echo "Creating fifo"
+echo "Protected fifos status: $(cat /proc/sys/fs/protected_fifos)"
 
 # Use environment variables with defaults
-PIPE_FILE=${PIPE_FILE:-/tmp/audio/plexamp_fifo}
+PIPE_FILE=${PIPE_FILE:-./tmp/audio/plexamp.out}
 SINK_NAME=${SINK_NAME:-Plexamp}
 FORMAT=${FORMAT:-s16le}
 RATE=${RATE:-48000}
 
+echo "Creating Pulseaudio FIFO at ${PIPE_FILE}"
+# Create the directory if it doesn't exist
+mkdir -p $(dirname ${PIPE_FILE})
+
+# Create the FIFO
+mkfifo ${PIPE_FILE}
+
+# Load the module
+echo "Loading Pulseaudio module-pip-sink module"
 pactl load-module module-pipe-sink file=${PIPE_FILE} sink_name=${SINK_NAME} format=${FORMAT} rate=${RATE}
+
+echo "Setting permissions for ${PIPE_FILE}"
 chmod 777 ${PIPE_FILE}
+
+echo "Setting default sink to ${SINK_NAME}"
+pactl set-default-sink ${SINK_NAME}
 
 echo "List sinks"
 pactl list short sinks
